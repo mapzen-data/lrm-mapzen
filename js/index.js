0ce5fe1dd2589c9a7ec8b19f7b043ec78602be28
Nathaniel Kelso <nvkelso@gmail.com>
2016-05-17 23:05:25 -0700

    large reorg to enable map interactivity for bike content

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-var map = L.map('map');
+var map = L.map('map', {"keyboardZoomOffset" : .05, "scrollWheelZoom": false });
+var picking = false;
+
+// usually we refer to key alone to make use of keymaster but
+// the key global conflicts with something in the Leaflet namespace, so enable special mode
+var key_nc = key.noConflict();
 
 var hashControl = new HashControl();
 
@@ -9,9 +14,258 @@ if (hasWebGL()) {
     scene: 'https://raw.githubusercontent.com/mapzen-data/te-bike-lrm-mapzen/gh-pages/map/te-bike-map.yaml',
     attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
   });
-  layer.addTo(map);
+
+  window.layer = layer;
+  var scene = layer.scene;
+  window.scene = scene;
+  var latlng = {};
+  var popup = document.getElementById('popup'); // click-popup
+
+  // Feature selection
+  function initFeatureSelection () {
+    map.getContainer().addEventListener('mousemove', function (event) {
+      picking = true;
+      latlng = map.mouseEventToLatLng(event);
+
+      var pixel = { x: event.clientX, y: event.clientY };
+
+      scene.getFeatureAt(pixel).then(function(selection) {
+        var url = 'https://www.openstreetmap.org/edit?';
+        var position = '19' + '/' + latlng.lat + '/' + latlng.lng;
+        var properties = null;
+
+        if (!selection || selection.feature == null || selection.feature.properties == null) {
+          picking = false;
+          popup.style.visibility = 'hidden';
+          return;
+        }
+
+        // We used to return if we weren't selectable, but now we
+        try {
+          properties = selection.feature.properties;
+
+          // default to OSM node, and zoom 19
+          osm_type = 'node';
+          osm_zoom = '19'
+
+          // If it's a way (eg road or landuse polygon), then those are usually large, so zoom out a bit
+          if( properties.sort_key ) {
+            osm_type = 'way';
+            osm_zoom = Math.max( 17, map.getZoom() );
+          }
+
+          // If it's a relation (usually landuse), then those are usually large, so zoom out a bit more
+          osm_id = properties.id;
+          if( osm_id < 0 ) {
+            osm_type = 'relation'
+            osm_id = Math.abs( osm_id );
+            osm_zoom = Math.max( 16, map.getZoom() );
+          }
+
+          // Update the OSM iD editor URL
+          url += osm_type + '=' + osm_id;
+        } catch(e) { /*console.log( 'no feature at pixel' );*/ }
+
+        url += '#map=' + position;
+
+        var josmUrl = 'http://www.openstreetmap.org/edit?editor=remote#map='+position;
+
+        popup.style.width = '250px';
+        popup.style.left = (pixel.x + 0) + 'px';
+        popup.style.top = (pixel.y + 0) + 'px';
+
+        var kind_words = ''
+        var named = '';
+
+        if( properties.name ) {
+          named = properties.name;
+        } else {
+          named = 'unnamed ' + properties.kind;
+        }
+
+        // What language do we want in the popup?
+        // Roads layer content first...
+        if( map.getZoom() >= 12 ) {
+          if( (properties.highway == 'cycleway' && properties.cycleway == 'sidepath' || properties.cycleway == 'segregated') ||
+              (properties.highway == 'footway'  && properties.bicycle == 'yes' && properties.segregated == 'yes' )
+          ) {
+            kind_words = named + " has a sidepath";
+          } else if( properties.highway == "cycleway" && properties.cycleway != 'sidepath' ) {
+            kind_words = named + " is a bike path";
+          } else if( properties.highway == "footway" && (properties.bicycle == 'designated' || properties.bicycle == 'yes') ) {
+            kind_words = named + " is a designated footway";
+          } else if( properties.highway == "steps" && (properties.bicycle == 'designated' || properties.bicycle == 'yes') ) {
+            kind_words = named + " is a designated steps";
+          } else if( properties.highway == 'track' && (properties.bicycle == 'designated' || properties.bicycle == 'yes') ) {
+            kind_words = named + " is a designated track";
+          } else if( properties.kind == 'path' && (properties.bicycle == 'designated' || properties.bicycle == 'yes') ) {
+            kind_words = named + " is a designated path";
+          } else if( properties.cycleway && properties.oneway == 'yes' ) {
+            kind_words = named + ' has a bike ' + properties.cycleway + " (implied right)";
+          } else if( properties.cycleway ) {
+            kind_words = named + ' has a bike ' + properties.cycleway + " (both sides)";
+          } else if( properties.cycleway_left && properties.cycleway_right ){
+            kind_words = named + ' has a bike ' + properties.cycleway_left + " (left) & " + properties.cycleway_right + " (right)";
+          } else if( properties.cycleway_left ){
+            kind_words = named + ' has a bike ' + properties.cycleway_left + " (left side)";
+          } else if ( properties.cycleway_right ){
+            kind_words = named + ' has a bike ' + properties.cycleway_right + " (right side)";
+          } else if ( properties.is_bicycle_related ) {
+            kind_words = named + " is a designated route only, does it have bike lanes?";
+          }
+          // ... now for POIs
+          else if( properties.kind == 'bicycle' ) {
+            kind_words = 'You found a bike shop to enhance!';
+          } else if( properties.kind == 'bicycle_rental' ) {
+                kind_words = 'You found a bike rental shop to enhance!';
+          } else if( properties.kind == 'bicycle_rental_station' ) {
+                kind_words = 'You found a bike rental station to enhance!';
+          } else if( properties.kind == 'bicycle_parking' ) {
+                kind_words = 'You found bike parking to enhance!';
+          } else if( properties.kind == 'cycle_barrier' ) {
+                kind_words = 'You found a bike barrier to enhance!';
+          } else if( properties.kind == 'bicycle_junction' ) {
+                kind_words = 'You found a bike junction to enhance!';
+          } else if( properties.kind == 'bicycle_repair_station' ) {
+            kind_words = 'You found a bike repair station to enhance!';
+          //
+          // Only show this when very far zoomed in
+          // As it's confusing to have the popup flashing too much, and
+          // enabling it farther out already conflicts with leaflet map pin dragging UX
+          } else if(map.getZoom() >= 15) {
+            kind_words = named + ' is not bike related, should it be?';
+          }
+
+          if( kind_words != '' ) {
+            popup.innerHTML = '<span class="labelInner">' + kind_words + '</span><br>';
+            popup.appendChild(createEditLinkElement(url, 'iD', 'Edit with iD ➹'));
+            popup.appendChild(createEditLinkElement(josmUrl, 'JOSM', 'Edit with JOSM ➹'));
+            popup.style.visibility = 'visible';
+          } else {
+            popup.style.visibility = 'hidden';
+          }
+        }
+      });
+    });
+  }
+
+  function createEditLinkElement (url, type, label) {
+    var el = document.createElement('div');
+    var anchor = document.createElement('a');
+    el.className = 'labelInner';
+    anchor.href = url;
+    anchor.target = '_blank';
+    anchor.textContent = label;
+    anchor.addEventListener('click', function (event) {
+      trackOutboundLink(url, 'editing_biking_features', type);
+    }, false);
+    el.appendChild(anchor);
+    return el;
+  }
+
+  /**
+  * Function that tracks a click on an outbound link in Google Analytics.
+  * This function takes a valid URL string as an argument, and uses that URL string
+  * as the event label. Setting the transport method to 'beacon' lets the hit be sent
+  * using 'navigator.sendBeacon' in browser that support it.
+  */
+  function trackOutboundLink (url, post_name, editor) {
+     // ga('send', 'event', [eventCategory], [eventAction], [eventLabel], [eventValue], [fieldsObject]);
+     ga('send', 'event', 'outbound', post_name, url, {
+     'transport': 'beacon',
+     // If opening a link in the current window, this opens the url AFTER
+     // registering the hit with Analytics. Disabled because here want the
+     // link to open in a new window, so this hit can occur in the current tab.
+     //'hitCallback': function(){document.location = url;}
+     });
+  }
+
+  // helper functions used in debug to fetch tile coordinate JSON files
+  function long2tile(lon,zoom) { return (Math.floor((lon+180)/360*Math.pow(2,zoom))); }
+  function lat2tile(lat,zoom)  { return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom))); }
+
+  function mapzenTileURL() {
+    // find minimum max_zoom of all sources
+    var max_zoom = 21;
+    for (source in scene.config.sources) {
+      if (scene.config.sources.hasOwnProperty(source)) {
+        if (scene.config.sources[source].max_zoom != "undefined") {
+          max_zoom = Math.min(max_zoom, scene.config.sources[source].max_zoom);
+        }
+      }
+    }
+    var zoom = max_zoom < map.getZoom() ? max_zoom : Math.floor(map.getZoom());
+    var tileCoords = { x : long2tile(latlng.lng,zoom), y: lat2tile(latlng.lat,zoom), z: zoom };
+
+    var url = 'http://vector.mapzen.com/osm/all/' + zoom + '/' + tileCoords.x  + '/' + tileCoords.y + '.topojson?api_key=vector-tiles-HqUVidw';
+    return url;
+  }
+
+  /***** Render loop *****/
+
+  function addGUI() {
+    // Link to edit in OSM - hold 'e' and click
+    map.getContainer().addEventListener('dblclick', function (event) {
+      //console.log( 'dblclick was had' );
+      if( timer ) { clearTimeout( timer ); timer = null; }
+      popup.style.visibility = 'hidden';
+    });
+
+    var timer;
+
+    map.getContainer().addEventListener('click', function (event) {
+      //console.log( 'click was had' );
+      if( timer ) { clearTimeout( timer ); timer = null; }
+      timer = setTimeout( function(){
+        picking = true;
+        latlng = map.mouseEventToLatLng(event);
+
+        var pixel = { x: event.clientX, y: event.clientY };
+        var url = 'https://www.openstreetmap.org/edit?';
+        var osm_zoom = Math.max( 16, map.getZoom() );
+
+        // position the map so it's at a similar zoom to Tangram
+        if (latlng) {
+          url += '#map=' + osm_zoom + '/' + latlng.lat + '/' + latlng.lng;
+              //console.log( 'url to get ' + url );
+        }
+
+        // Quick view tile JSON source debug
+        if( key_nc.cmd || key_nc.alt ) {
+          window.open( mapzenTileURL(), '_blank' );
+        // Edit the map area immediately in OSM's iD editor
+        // If you want to edit a specific feature, use the mouseover section
+        // to specify whitelist of features kinds.
+        } else if( key_nc.shift ) {
+            window.open(url, '_blank');
+        }
+        timer = null;
+      }, 200 );
+    });
+  }
+
+  function inIframe () {
+    try {
+      return window.self !== window.top;
+    } catch (e) {
+      return true;
+    }
+  }
+
+  window.addEventListener('load', function () {
+    // If not embedded in a blog post, enable the mouse scroll wheel zoom 
+    if (!inIframe()) {
+      map.scrollWheelZoom.enable();
+    }
+    // enable debug keyboard shortcuts
+    addGUI();
+    // enable mouse over popup windows
+    initFeatureSelection();
+    // finally add the layer to the map
+    layer.addTo(map);
+  });
 } else {
-  // Use normal OSM tiles instead of Tangram when there is no webgl available
+  // Use normal OSM raster tiles instead of Tangram when WebGL is not available
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
    }).addTo(map);
